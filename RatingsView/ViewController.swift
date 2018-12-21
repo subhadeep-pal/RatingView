7b0e7dd0b6b820aaeece7be8082e566837e34c5e
Subhadeep Pal <subhadeepal@gmail.com>
2018-12-21 16:36:25 +0530

    Initial Commit

---
+++ RatingsView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RatingsView
+//
+//  Created by Subhadeep Pal on 21/12/18.
+//  Copyright © 2018 Subhadeep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
